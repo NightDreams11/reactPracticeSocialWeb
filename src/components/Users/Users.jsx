1b2bef6922c3fd919cbc65515d5c9229cb10e1f0
NightDreams <rost090909@gmail.com>
2021-06-15 20:58:40 +0300

    57 lesson progress

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,54 +1,48 @@
-import * as axios from 'axios'
 import React from 'react'
-import styles from './Users.module.css'
 import userPhoto from '../../assets/images/photo.jpg';
+import styles from './Users.module.css'
 
-class Users extends React.Component {
+const Users = (props) => {
 
-    // constructor(props) {
-    //     super(props);
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-    // }
-
-    componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            this.props.setUsers(response.data.items);
-        });
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
     }
 
-    getUsers = () => {
 
-
-    }
-
-    render() {
-        return <div>
-            {
-                this.props.users.map(u => <div key={u.id}>
-                    <span>
-                        <div>
-                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}></img>
-                        </div>
-                        <div>
-                            {u.followed ? <button onClick={() => { this.props.unfollow(u.id) }}>unfollow</button> : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
-                        </div>
-                    </span>
-                    <span>
-                        <span>
-                            <div>{u.name}</div>
-                            <div>{u.status}</div>
-                        </span>
-                        <span>
-                            {/* <div>{u.location.country}</div> */}
-                            {/* <div>{u.location.city}</div> */}
-                            <div></div>
-                            <div></div>
-                        </span>
-                    </span>
-                </div>)
-            }
+    return <div>
+        <div>
+            {pages.map(p => {
+                return <span className={props.currentPage === p && styles.selectedPage} onClick={(e) => { props.onPageChanged(p) }}>{p}</span>
+            })}
         </div>
-    }
+        {
+            props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}></img>
+                    </div>
+                    <div>
+                        {u.followed ? <button onClick={() => { props.unfollow(u.id) }}>unfollow</button> : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                    </div>
+                </span>
+                <span>
+                    <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                        {/* <div>{u.location.country}</div> */}
+                        {/* <div>{u.location.city}</div> */}
+                        <div></div>
+                        <div></div>
+                    </span>
+                </span>
+            </div>)
+        }
+    </div>
 }
 
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,11 +1,18 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
+const TOGGLE_IS_FETCHNG = 'TOGGLE_IS_FETCHNG'
 
 let initialState = {
     users: [
 
-    ]
+    ],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1,
+    isFetching: false
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -33,7 +40,16 @@ const usersReducer = (state = initialState, action) => {
             }
 
         case SET_USERS:
-            return { ...state, users: [...state.users, ...action.users] }
+            return { ...state, users: action.users }
+
+        case SET_CURRENT_PAGE:
+            return { ...state, currentPage: action.currentPage }
+
+        case SET_TOTAL_USERS_COUNT:
+            return { ...state, totalUsersCount: action.count }
+
+        case TOGGLE_IS_FETCHNG:
+            return { ...state, isFetching: action.isFetching }
 
         default:
             return state;
@@ -44,5 +60,8 @@ export const followAC = (userId) => ({ type: FOLLOW, userId })
 
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
 export const setUsersAC = (users) => ({ type: SET_USERS, users })
+export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage })
+export const setUsersTotalCountAC = (totalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, count: totalUsersCount })
+export const toggleIsFetchingAC = (isFetching) => ({ type: TOGGLE_IS_FETCHNG, isFetching })
 
 export default usersReducer;
