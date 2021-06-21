0c84549138da179f11adc12604070ab776f042ac
NightDreams <rost090909@gmail.com>
2021-06-21 18:39:29 +0300

    63 lessson progress

---
+++ src/api/api.jsx
@@ -0,0 +1,29 @@
+import * as axios from 'axios'
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    headers: {
+        "API-KEY": "4e078e26-7b0e-477a-9a26-66473c5449f5"
+    }
+})
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`,
+            {
+                withCredentials: true
+            })
+            .then(response => {
+                return response.data
+            })
+    }
+}
+
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+    return instance.get(`follow?page=${currentPage}&count=${pageSize}`)
+        .then(response => {
+            return response.data
+        })
+}
