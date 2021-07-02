1b04854ed506478ff29e7e22f1155bebf8bcafb6
NightDreams <rost090909@gmail.com>
2021-07-02 23:51:40 +0300

    71 lesson progress

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import classes from './ProfileInfo.module.css'
 import Preloader from '../../common/Preloader/Preloader';
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
 
-  if(!props.profile){
+  if (!props.profile) {
     return <Preloader></Preloader>
   }
 
   return (
     <div>
       <div>
-        <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'></img>
+        {/* <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'></img> */}
       </div>
       <div className={classes.descriptionBlock} src='https://blog.matcharesident.com/wp-content/uploads/2019/07/iStock-944453634-750x450.jpg'>
         <img src={props.profile.photos.large}></img>
-        ava + discr
+        <ProfileStatus status={"Hello"}></ProfileStatus>
       </div>
     </div >
   )
